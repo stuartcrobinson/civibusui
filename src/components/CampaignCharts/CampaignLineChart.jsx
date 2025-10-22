a686c87801aa2c5e6245379bc2a025e30c05b485
Stuart Robinson <stuart@Stuarts-MBP.lan>
2025-10-22 12:41:28 -0400

    taller line graphs 600

--- src/components/CampaignCharts/CampaignLineChart.jsx
+++ src/components/CampaignCharts/CampaignLineChart.jsx
@@ -236,7 +236,7 @@ function CampaignLineChart({
         </div>
       </div>
       
-      <ResponsiveContainer width="100%" height={400}>
+      <ResponsiveContainer width="100%" height={600}>
         <LineChart data={chartData} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
           <CartesianGrid strokeDasharray="3 3" stroke="#e5e7eb" />
           <XAxis 
